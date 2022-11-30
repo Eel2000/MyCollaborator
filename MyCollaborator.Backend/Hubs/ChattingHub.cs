7cb997b2ade416ea5c731d605e5bb90b5b586e32
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2022-11-30 16:03:32 +0100

    ChattingHub.cs implementation

--- MyCollaborator.Backend/Hubs/ChattingHub.cs
+++ MyCollaborator.Backend/Hubs/ChattingHub.cs
@@ -1,9 +1,33 @@
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using MyCollaborator.Backend.Contexts;
 using MyCollaborator.Backend.Hubs.Interfaces;
+using MyCollaborator.Shared.Models;
 
 namespace MyCollaborator.Backend.Hubs;
 
 public class ChattingHub : Hub<IChattingHub>
 {
+    private readonly ApplicationDbContext _context;
+
+    public ChattingHub(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async ValueTask BroadCast(Message message)
+    {
+        await _context.Message.AddAsync(message);
+        await Clients.Others.ReceiveFromAll(message);
+        await _context.SaveChangesAsync();
+    }
+
+    public async ValueTask SpecificSend(Message message)
+    {
+        var to = await _context.UserConnection
+            .Where(u => u.UserId == message.To)
+            .ToListAsync();
+        await Clients.Clients(to.Select(t => t.Connection)).ReceiveFromFriend(message);
+    }
     
 }
